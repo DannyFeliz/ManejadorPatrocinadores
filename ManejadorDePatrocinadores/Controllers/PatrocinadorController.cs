d39ec28214c13e63ab9f9df5755f212c63cb10d3
Justi <justinsencion@gmail.com>
2017-10-30 00:06:10 -0400

    Add Patrocinadores module

---
+++ ManejadorDePatrocinadores/Controllers/PatrocinadorController.cs
@@ -0,0 +1,101 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System.Web.Mvc;
+
+namespace ManejadorDePatrocinadores.Controllers
+{
+    public class PatrocinadorController : Controller
+    {
+        // GET: Patrocinador
+        public ActionResult Index()
+        {
+            ViewData["patrocinadores"] = Models.Patrocinador.obtenerTodos();
+            return View();
+        }
+
+        // GET: Patrocinador/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Patrocinador/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Patrocinador/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                using (var connection = Utils.Db.Connection())
+                {
+                    connection.Open();
+
+                    SqlCommand cmd = new SqlCommand("insertar_patrocinador", connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("numero_contrato", collection.GetValue("numero_contrato").AttemptedValue);
+                    cmd.Parameters.AddWithValue("nombre", collection.GetValue("nombre").AttemptedValue);
+                    cmd.Parameters.AddWithValue("duracion_contrato", collection.GetValue("duracion_contrato").AttemptedValue);
+                    cmd.Parameters.AddWithValue("importe_contrato", collection.GetValue("importe_contrato").AttemptedValue);
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+                ViewData["patrocinadores"] = Models.Patrocinador.obtenerTodos();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Patrocinador/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Patrocinador/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Patrocinador/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Patrocinador/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ManejadorDePatrocinadores/Models/Patrocinador.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ManejadorDePatrocinadores.Models
+{
+    public class Patrocinador
+    {
+        [DisplayName("Id")]
+        public int numero_contrato { get; set; }
+        [DisplayName("Nombre")]
+        [MaxLength(75)]
+        public string nombre { get; set; }
+        [DisplayName("Duracion del Contrato")]
+        public int duracion_contrato { get; set; }
+        [DisplayName("Importe del Contrato")]
+        public float importe_contrato { get; set; }
+
+        public static List<Patrocinador> obtenerTodos()
+        {
+            var connection = Utils.Db.Connection();
+            var list = new List<Patrocinador>();
+
+            using (var query = connection.CreateCommand())
+            {
+                query.CommandText = "EXEC lista_patrocinadores";
+                connection.Open();
+                using (var row = query.ExecuteReader())
+                {
+                    while (row.Read())
+                    {
+                        var patrocinadores = new Patrocinador
+                        {
+                            numero_contrato = Convert.ToInt32(row.GetValue(row.GetOrdinal("numero_contrato"))),
+                            nombre = row.GetValue(row.GetOrdinal("nombre")).ToString(),
+                            duracion_contrato = Convert.ToInt32(row.GetValue(row.GetOrdinal("duracion_contrato"))),
+                            importe_contrato = Convert.ToInt32(row.GetValue(row.GetOrdinal("importe_contrato"))),
+                        };
+                        list.Add(patrocinadores);
+                    }
+                }
+                connection.Close();
+            }
+
+            return list;
+        }
+    }
+}
